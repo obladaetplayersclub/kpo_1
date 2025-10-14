f928637420fd0651760024dedb27658d10cae9d6
konstantinokriashvili <konstantinokriashvili2006@gmail.com>
2025-10-15 00:47:29 +0300

    Добавлена проверка ввода числа, чтобы прога не крашнулась

--- src/main/java/zoo/ZooApplication.java
+++ src/main/java/zoo/ZooApplication.java
@@ -14,6 +14,7 @@ import zoo.domain.animals.inzoo.Rabbit;
 import zoo.domain.animals.inzoo.Tiger;
 import zoo.domain.animals.inzoo.Wolf;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -70,7 +71,6 @@ public class ZooApplication implements CommandLineRunner {
                 case 0: {
                     running = false;
                 }
-
                 default: {
                     System.out.println("Неверный пункт!");
                 }
@@ -81,46 +81,53 @@ public class ZooApplication implements CommandLineRunner {
 
     private void addAnimalFlow(Scanner in) {
         System.out.println("Травоядное (1) или хищник (2)?");
-        int kind = in.nextInt();
+        int kind = checknumber(in);
 
         Animals animal;
         if (kind == 1) {
             System.out.println("Кролик (1) или Обезьяна (2)?");
-            int ht = in.nextInt();
+            int ht = checknumber(in);
 
             System.out.print("Имя: ");
             String name = in.next();
 
             System.out.print("Кол-во кг еды: ");
-            int food = in.nextInt();;
+            int food = checknumber(in);
 
             System.out.print("Доброта (0–10): ");
-            int happiness = in.nextInt();;
+            int happiness = checknumber(in);
 
-            animal = (ht == 1)
-                    ? new Rabbit(name, food, happiness)
-                    : new Monkey(name, food, happiness);
+            animal = (ht == 1) ? new Rabbit(name, food, happiness)  : new Monkey(name, food, happiness);
 
         } else if (kind == 2) {
             System.out.println("Волк (1) или Тигр (2)?");
-            int pt = in.nextInt();
+            int pt = checknumber(in);
 
             System.out.print("Имя: ");
             String name = in.next();
 
-            System.out.print("Еда/сутки (кг): ");
-            int food = in.nextInt();
-
-
-            animal = (pt == 1)
-                    ? new Wolf(name, food)
-                    : new Tiger(name, food);
+            System.out.print("Кол-во кг еды: ");
+            int food = checknumber(in);
+            animal = (pt == 1) ? new Wolf(name, food) : new Tiger(name, food);
         } else {
-            System.out.println("Нужно выбрать 1 или 2.");
+            System.out.println("Нужно выбрать 1 или 2!");
             return;
         }
 
         zoo.addAnimal(animal);
         contact.addContact(animal);
     }
+
+    private int checknumber(Scanner in){
+        while (true) {
+            try {
+                return in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.print("Ошибка ввода! Вы должны ввести число \n" + "Введите число: ");
+                in.nextLine();
+            }
+        }
+    }
+
+
 }
